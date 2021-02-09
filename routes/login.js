b7a568ed3ea19844c03bf782736e8ba535f576e5
Jinxyz <xxyaymanyayxx@hotmail.com>
2021-02-09 11:08:40 +0100

    feat: login

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET login form */
 router.get('/', function(req, res, next) {
   res.render('login', {title: 'skolfoft'});
 });
+
+/* POST login */
+router.post('/', function(req, res, next) {
+  
+  console.log(req.body);
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (password == "ABCD"){
+    res.send('Victory!')
+  }
+    else {
+      res.send('You lose! LOSER');
+    }
+
+});
+
 
 module.exports = router;
