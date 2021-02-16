27ae5ac3512c7bf93653fa091c0be0a49e816b59
Jinxyz <xxyaymanyayxx@hotmail.com>
2021-02-16 11:02:23 +0100

    Express-validator added
    Fixed so you can't log in with every password

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const { query } = require('../models/db');
+const { body, validationResult } = require('express-validator');
 
 /* GET login form */
 router.get('/', function(req, res, next) {
@@ -22,55 +23,42 @@ router.get('/kryptan/:pwd', function(req, res, next) {
 });
 
 /* POST login */
-router.post('/', async function(req, res, next) {
+router.post('/', 
+  body('username').notEmpty().trim(), 
+  body('password').notEmpty(),
+  async function(req, res, next) {
   
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render('login',{ errors: errors.array()});
+    }
+
   console.log(req.body);
 
   const username = req.body.username;
   const password = req.body.password;
 
-  if (username && password) {
     try{
       const sql = 'SELECT password FROM users WHERE name = ?';
       const result = await query(sql, username);
       
-
       if (result.length > 0) {
         bcrypt.compare(password, result[0].password, function(err, result) {
-          if (result = true) {
+          if (result == true) {
             req.session.loggedin = true;
             req.session.username = username;
-            res.redirect('/sekret')
+            res.redirect('/sekret');
           } else {
-            res.render('login', {error : 'Wrong username or password!'});
+            res.render('login', {errors : 'Wrong username or password!'});
           }
         });
       } else {
-        res.render('login', {error : 'Wrong username or password!'});
+        res.render('login', {errors : 'Wrong username or password!'});
       } 
     } catch (e) {
       next(e);
       console.error(e);
     } 
-  } else {
-    res.render('login', {error : 'Wrong username or password!'});
-  } 
 });
-//     if {
-//     // kolla om login stämmer
-//     req.session.loggedin = true;
-//     req.session.username = username;
-//     res.redirect('/sekret');
-
-//   }
-//     else {
-//       // kommentera ut vid felsökning
-//       res.render('login', 
-//       {
-//         title: 'skolfoft',
-//         error: 'FEL!'
-//       });
-//     }
-
 
 module.exports = router;
